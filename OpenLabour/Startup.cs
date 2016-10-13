c8304ee6f0f4d66d4b803a6e8c26a825132d10c5
arun <arun@VAIO>
2016-10-13 02:27:54 -0700

    Add project files.

---
+++ OpenLabour/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OpenLabour.Startup))]
+namespace OpenLabour
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
