016e81897422cd9f7b88c5384fe50838d14206eb
arun <arun@VAIO>
2016-10-16 16:14:41 +0530

    sdsd

--- OpenLabour/Migrations/ContextA/201610151819362_1.cs
+++ OpenLabour/Migrations/ContextA/201610151819362_1.cs
@@ -8,7 +8,7 @@ namespace OpenLabour.Migrations.ContextA
         public override void Up()
         {
             CreateTable(
-                "dbo.AspNetRoles",
+                "dbo.AspNetRoles", 
                 c => new
                     {
                         Id = c.String(nullable: false, maxLength: 128),
